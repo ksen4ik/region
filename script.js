8e891e3303d100ed116665920d99505e5c992e66
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2024-01-22 15:37:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-// Получение IP адреса
-// fetch('https://api.ipify.org?format=json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const ipAddress = data.ip;
-//     console.log(ipAddress);
-//   })
-//   .catch(error => {
-//     console.error('Ошибка:', error);
-//   });
-
-// // Получение региона
-// fetch('https://ipapi.co/json/')
-//   .then(response => response.json())
-//   .then(data => {
-//     const region = data.region;
-//     console.log(region);
-//   })
-//   .catch(error => {
-//     console.error('Ошибка:', error);
-//   });
-
-// Получение кода региона Беларуси
+// Получение региона
 fetch('https://ipapi.co/json/')
   .then(response => response.json())
   .then(data => {
     const country = data.country;
-    const regionCode = data.region_code;
-
+    const region = data.region;
     if (country === 'BY') {
-      console.log(regionCode);
+      console.log(region);
     } else {
-      console.log('Не удалось определить код региона Беларуси');
+      console.log('Не удалось определить регион Беларуси');
     }
   })
   .catch(error => {
